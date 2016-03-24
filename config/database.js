be599a0d6cb72a32981a5e97e9e4a3f7efa5a61e
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2016-03-23 19:23:11 -0700

    fixed untracked files

--- config/database.js
+++
@@ -1,8 +0,0 @@
-
-// config/database.js
-// url to database
-module.exports = {
-    'url' : "mongodb://beb619:gattaca94@jello.modulusmongo.net:27017/eziran3U"
-    // 'url' : 'mongodb://localhost:27017/vitamins'
-    // 'postgres:///vitamins'
-};
