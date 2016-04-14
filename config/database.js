f48ddfee0f3356a0a3d36bcb4db76fcb6cea5f4c
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2016-04-14 11:02:44 -0700

    added database.js back into config, used -f to override gitignore

---
+++ config/database.js
@@ -0,0 +1,8 @@
+
+// config/database.js
+// url to database
+module.exports = {
+    'url' : "mongodb://beb619:gattaca94@jello.modulusmongo.net:27017/eziran3U"
+    // 'url' : 'mongodb://localhost:27017/vitamins'
+    // 'postgres:///vitamins'
+};
