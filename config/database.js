019fbce26f76171d460e880981c6563347a01f9e
Brandon Beckhardt <brandon.beckhardt5@gmail.com>
2017-03-27 19:26:01 -0700

    Added options for db url

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@
 // config/database.js
 // url to database
 module.exports = {
-    'url' : 'mongodb://custom_vits:nYKc3ImemqnN8Iew@customvitamin1-shard-00-00-nmj7t.mongodb.net:27017,customvitamin1-shard-00-01-nmj7t.mongodb.net:27017,customvitamin1-shard-00-02-nmj7t.mongodb.net:27017/custom_vitamins?ssl=true&replicaSet=CustomVitamin1-shard-0&authSource=admin'
+    'url' : 'mongodb://custom_vits:nYKc3ImemqnN8Iew@customvitamin1-shard-00-00-nmj7t.mongodb.net:27017,customvitamin1-shard-00-01-nmj7t.mongodb.net:27017,customvitamin1-shard-00-02-nmj7t.mongodb.net:27017/custom_vitamins?ssl=true&replicaSet=CustomVitamin1-shard-0&authSource=admin' //Prod
+    // 'url':'mongodb://localhost:vitamins_1' //local dev
 };
